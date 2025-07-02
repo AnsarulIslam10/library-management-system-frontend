e3ab0ada55fe4a131a9f1c76db1b1ebcc7220e60
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-02 08:46:09 +0600

    show all books in table format

--- src/pages/BookListPage.tsx
+++ src/pages/BookListPage.tsx
@@ -1,4 +1,13 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { useGetBooksQuery } from "@/redux/api/libraryApi";
+import { Pen, Trash2 } from "lucide-react";
 
 export default function BookListPage() {
   const { data: books, error, isLoading } = useGetBooksQuery({});
@@ -11,9 +20,36 @@ export default function BookListPage() {
   }
   return (
     <div>
-      {books?.map((book) => (
-        <p key={book._id}>{book.title}</p>
-      ))}
+      <h1 className="text-center text-3xl font-semibold underline mb-5">Books List</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Title</TableHead>
+            <TableHead>Author</TableHead>
+            <TableHead>Genre</TableHead>
+            <TableHead>ISBN</TableHead>
+            <TableHead>Copies</TableHead>
+            <TableHead>Availability</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {books?.map((book) => (
+            <TableRow key={book._id}>
+              <TableCell className="font-medium">{book.title}</TableCell>
+              <TableCell>{book.author}</TableCell>
+              <TableCell>{book.genre}</TableCell>
+              <TableCell>{book.isbn}</TableCell>
+              <TableCell>{book.copies}</TableCell>
+              <TableCell>{book.available ? 'Available' : 'Unavailable'}</TableCell>
+              <TableCell className="flex items-center gap-2">
+                <Pen className="cursor-pointer" />
+                <Trash2 className="cursor-pointer text-red-500" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }

93fbbbb8775c3b22e45bb1b145939bab6bee5b59
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-07-02 23:08:38 +0600

    simple footer

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className="text-center py-3 bg-gray-100">Created by Ansarul Islam</div>;
+}
--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 export default function MainLayout() {
   return (
     <div className="max-w-7xl mx-auto px-2">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
